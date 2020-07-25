605abee05458d4e7ee7114370195c542f0d0eaac
Anggih Septiawan <albertanggih@gmail.com>
2020-07-25 16:32:03 +0700

    add database

--- dump_database/admin_desa.sql
+++ dump_database/admin_desa.sql
@@ -11,9 +11,55 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Data exporting was unselected.
+-- Dumping structure for table admin_desa.admin
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `image` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(250) NOT NULL DEFAULT '',
+  `is_active` int(11) NOT NULL DEFAULT '1',
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table admin_desa.admin: ~1 rows (approximately)
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` (`id_admin`, `name`, `email`, `image`, `password`, `is_active`, `created_at`) VALUES
+	(3, 'Admin', 'admin@gmail.com', 'default.jpg', '$2y$10$umxUroKR2lDI11kiKFZz9uzr5PtQ4rS.tvxvSn1qtRSb4J9cY44HW', 1, '0000-00-00');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+
+-- Dumping structure for table admin_desa.panduan
+CREATE TABLE IF NOT EXISTS `panduan` (
+  `panduan_id` int(11) NOT NULL AUTO_INCREMENT,
+  `judul` varchar(250) NOT NULL,
+  `path` varchar(250) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`panduan_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table admin_desa.panduan: ~4 rows (approximately)
+/*!40000 ALTER TABLE `panduan` DISABLE KEYS */;
+INSERT INTO `panduan` (`panduan_id`, `judul`, `path`, `created_at`) VALUES
+	(3, 'Test upload file 1', './uploads/2020/07/5652-PBO_-_Week_01_-_Java_Intro.pdf', '2020-07-23 14:46:05'),
+	(4, 'Test upload file 2', './uploads/2020/07//5130-', '2020-07-23 15:02:24'),
+	(5, 'Test upload file 3', './uploads/2020/07//3205-', '2020-07-23 15:07:34');
+/*!40000 ALTER TABLE `panduan` ENABLE KEYS */;
+
+-- Dumping structure for table admin_desa.surat
+CREATE TABLE IF NOT EXISTS `surat` (
+  `id_surat` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `created_at` date NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_surat`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table admin_desa.surat: ~0 rows (approximately)
+/*!40000 ALTER TABLE `surat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `surat` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
