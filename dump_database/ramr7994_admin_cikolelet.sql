1e96836a438d5f8971efed217760c31a93547097
Harun Al Rosyid/170101010 <38546311+harun248@users.noreply.github.com>
2021-06-29 20:56:54 +0700

    update user

--- dump_database/ramr7994_admin_cikolelet.sql
+++ dump_database/ramr7994_admin_cikolelet.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 19, 2020 at 12:01 AM
--- Server version: 10.2.33-MariaDB-cll-lve
--- PHP Version: 7.3.6
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 29, 2021 at 01:55 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ramr7994_admin_cikolelet`
+-- Database: `cikolelet`
 --
 
 -- --------------------------------------------------------
@@ -28,15 +27,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '',
   `emails` varchar(50) NOT NULL DEFAULT '',
   `image` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(250) NOT NULL DEFAULT '',
-  `is_active` int(11) NOT NULL DEFAULT 1,
-  `created_at` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_active` int(11) NOT NULL DEFAULT '1',
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -47,7 +48,8 @@ INSERT INTO `admin` (`id_admin`, `name`, `emails`, `image`, `password`, `is_acti
 (4, 'Dodi mulyono', 'dodi@gmail.com', 'default.jpg', '$2y$10$zb.tnYvCzaSFmXLQ8PybWezbyfrp/VlzeTxVSELsz.952WMiu17TK', 1, '0000-00-00'),
 (5, 'Test', 'Test1@email.com', 'default.jpg', '$2y$10$we7q/L0JCqBIl9tb.bcsnOdyuUYr23awc2JCk08QWZSV9P1ihO9oy', 1, '0000-00-00'),
 (6, 'Admin', 'admincikolelet@gmail.com', 'default.jpg', '$2y$10$3MZ8gJPMFG/sEVHqJ.fc6ulaPH81AL2widy1HnhW08lJ1Mf3wpFiO', 1, '0000-00-00'),
-(7, 'Demo', 'demoadmin@gmail.com', 'default.jpg', '$2y$10$UCx0DaWXSZjm4TM4w7YaoOXgSqV6xrBCX4IyXV6lOCiDUJjXVFTMO', 1, '0000-00-00');
+(7, 'Demo', 'demoadmin@gmail.com', 'default.jpg', '$2y$10$UCx0DaWXSZjm4TM4w7YaoOXgSqV6xrBCX4IyXV6lOCiDUJjXVFTMO', 1, '0000-00-00'),
+(8, 'Admin', 'admins@gmail.com', 'default.jpg', '$2y$10$lMBgH0gwvjoGZTIYBufmf.4WG5zeWtqUx31qKBZ.dzunE6g/ON.Oi', 1, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -55,11 +57,13 @@ INSERT INTO `admin` (`id_admin`, `name`, `emails`, `image`, `password`, `is_acti
 -- Table structure for table `jenis`
 --
 
-CREATE TABLE `jenis` (
-  `jenis_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `jenis`;
+CREATE TABLE IF NOT EXISTS `jenis` (
+  `jenis_id` int(11) NOT NULL AUTO_INCREMENT,
   `kategori` varchar(100) NOT NULL,
-  `nama` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `nama` varchar(100) NOT NULL,
+  PRIMARY KEY (`jenis_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jenis`
@@ -77,10 +81,12 @@ INSERT INTO `jenis` (`jenis_id`, `kategori`, `nama`) VALUES
 -- Table structure for table `kategori`
 --
 
-CREATE TABLE `kategori` (
-  `kategori_id` int(11) NOT NULL,
-  `kategori` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `kategori`;
+CREATE TABLE IF NOT EXISTS `kategori` (
+  `kategori_id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(100) NOT NULL,
+  PRIMARY KEY (`kategori_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kategori`
@@ -97,15 +103,17 @@ INSERT INTO `kategori` (`kategori_id`, `kategori`) VALUES
 -- Table structure for table `panduan`
 --
 
-CREATE TABLE `panduan` (
-  `panduan_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `panduan`;
+CREATE TABLE IF NOT EXISTS `panduan` (
+  `panduan_id` int(11) NOT NULL AUTO_INCREMENT,
   `judul` varchar(250) NOT NULL,
   `jenis` varchar(100) NOT NULL,
   `persyaratan` text NOT NULL,
   `mekanisme` text NOT NULL,
   `path` varchar(250) NOT NULL,
-  `created_at` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`panduan_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `panduan`
@@ -123,17 +131,19 @@ INSERT INTO `panduan` (`panduan_id`, `judul`, `jenis`, `persyaratan`, `mekanisme
 -- Table structure for table `pengajuan`
 --
 
-CREATE TABLE `pengajuan` (
-  `pengajuan_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `pengajuan`;
+CREATE TABLE IF NOT EXISTS `pengajuan` (
+  `pengajuan_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `jenis` varchar(50) NOT NULL,
   `keterangan` text NOT NULL,
   `path` varchar(255) NOT NULL,
   `path_admin` varchar(255) NOT NULL,
   `feedback` varchar(500) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` int(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`pengajuan_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pengajuan`
@@ -152,8 +162,9 @@ INSERT INTO `pengajuan` (`pengajuan_id`, `user_id`, `jenis`, `keterangan`, `path
 -- Table structure for table `profil_desa`
 --
 
-CREATE TABLE `profil_desa` (
-  `profil_desa_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `profil_desa`;
+CREATE TABLE IF NOT EXISTS `profil_desa` (
+  `profil_desa_id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(20) NOT NULL,
   `alamat` text NOT NULL,
   `telp` varchar(16) NOT NULL,
@@ -162,8 +173,9 @@ CREATE TABLE `profil_desa` (
   `total_pria` int(9) NOT NULL,
   `total_wanita` int(9) NOT NULL,
   `deskripsi` varchar(250) NOT NULL,
-  `gmaps` varchar(500) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `gmaps` varchar(500) NOT NULL,
+  PRIMARY KEY (`profil_desa_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `profil_desa`
@@ -178,8 +190,9 @@ INSERT INTO `profil_desa` (`profil_desa_id`, `nama`, `alamat`, `telp`, `email`, 
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(250) NOT NULL,
   `username` varchar(250) NOT NULL,
@@ -189,8 +202,9 @@ CREATE TABLE `user` (
   `gender` varchar(50) NOT NULL,
   `tmp_lahir` varchar(100) NOT NULL,
   `tgl_lahir` date NOT NULL,
-  `alamat` varchar(250) NOT NULL DEFAULT ''
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `alamat` varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -201,99 +215,9 @@ INSERT INTO `user` (`user_id`, `email`, `password`, `username`, `nohp`, `nik`, `
 (2, 'aldo@gmail.com', '$2y$10$IXTJoYJGu/sdkIqObABN2Oc84QpGpoJjv4.6G277wOhwt8EuEz.oa', 'aldo', '0939483984', '123456724567', 'Aldo Lesmana', 'laki-laki', 'Tegal', '1997-06-05', 'Jl. Kebayoran lama'),
 (3, 'anggih@gmail.com', '$2y$10$HRiHM8vl6YyCACz5JAFfk.v3j5T7jd5FCHLaxbrhPL5f52B8jQujO', 'Anggih', '02382837827', '1234567891234567', 'Anggih Septiawan', 'laki-laki', 'Tegal', '1995-09-02', 'Tangerang'),
 (4, 'alrosyid@gmail.com', '$2y$10$VyyEXzfELQiS5zAKjRUXP.2QGtU96asdlobcdVM/9Ra8uVDlp404K', 'alrosyid07', '085779995638', '3216111220596002', 'harun al rosyid', 'laki-laki', 'Jakarta', '1988-11-11', 'jakarta'),
-(5, 'Test@email.com', '$2y$10$2bi5fodeHhedEO.9bptCh.C1USgeYjJ0xE9RvivFzJwCO27fMGbzG', 'Test', '083215851255', '123123123123123', 'Test1', 'laki-laki', 'Jakarta', '2020-08-16', 'Jl. jembatan tinggi no.16');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indexes for table `jenis`
---
-ALTER TABLE `jenis`
-  ADD PRIMARY KEY (`jenis_id`);
-
---
--- Indexes for table `kategori`
---
-ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`kategori_id`);
-
---
--- Indexes for table `panduan`
---
-ALTER TABLE `panduan`
-  ADD PRIMARY KEY (`panduan_id`);
-
---
--- Indexes for table `pengajuan`
---
-ALTER TABLE `pengajuan`
-  ADD PRIMARY KEY (`pengajuan_id`);
-
---
--- Indexes for table `profil_desa`
---
-ALTER TABLE `profil_desa`
-  ADD PRIMARY KEY (`profil_desa_id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `jenis`
---
-ALTER TABLE `jenis`
-  MODIFY `jenis_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `kategori`
---
-ALTER TABLE `kategori`
-  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `panduan`
---
-ALTER TABLE `panduan`
-  MODIFY `panduan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `pengajuan`
---
-ALTER TABLE `pengajuan`
-  MODIFY `pengajuan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `profil_desa`
---
-ALTER TABLE `profil_desa`
-  MODIFY `profil_desa_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+(5, 'Test@email.com', '$2y$10$2bi5fodeHhedEO.9bptCh.C1USgeYjJ0xE9RvivFzJwCO27fMGbzG', 'Test', '083215851255', '123123123123123', 'Test1', 'laki-laki', 'Jakarta', '2020-08-16', 'Jl. jembatan tinggi no.16'),
+(6, 'alrosyid07@gmail.com', '$2y$10$0oStvfA3Moo4foc7VU/3be4ZVCvAqyf6dJU9bPYXx9YfYdVt0sXny', 'alrosyid07@gmail.com', '088989', '11212121212121', '1234567', 'perempuan', 'dd', '2021-06-24', 'Block 3B, Level 20, Unit 7, Plaza Sentral, Jalan Stesen Sentral 5, 50470 Kuala Lumpur, Malaysia'),
+(7, 'demouser@gmail.com', '$2y$10$Gkr9hjUBS6VbOZr4xcSWgewd/LoJp00sLMSdd3jC4PFuaGelo74gC', 'demo', '12345678', '11212121212121', 'demo', 'laki-laki', 'dd', '2021-06-25', 'dd');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
